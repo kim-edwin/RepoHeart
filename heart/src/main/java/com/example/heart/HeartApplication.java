fcc676a6569455ddb2572416a506f30ee967dd89
Hyeonu Kim <woo813625@gmail.com>
2023-12-12 12:47:48 +0900

    생성 및 yml 적용
    Gridle 프로젝트 생성( 후 application.yml 적용(한글 깨짐, mysql db연결, mustache)

---
+++ heart/src/main/java/com/example/heart/HeartApplication.java
@@ -0,0 +1,13 @@
+package com.example.heart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartApplication.class, args);
+	}
+
+}
