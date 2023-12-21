582ace0f0fbabc6be0d10e17c5f783426bba9e00
Hyeonu Kim <woo813625@gmail.com>
2023-12-21 14:02:15 +0900

    미니프로젝트 최종수정 & 종료

--- heart/src/main/java/com/example/heart/database/dao/MainDao.java
+++ heart/src/main/java/com/example/heart/database/dao/MainDao.java
@@ -13,6 +13,9 @@ import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,8 +39,8 @@ public class MainDao {
 
     // pdf업로드
     public void uploadPdf(MultipartFile pdfFile) throws IOException {
-        // 로그인 기능이 없으므로 임시로 고정된 사용자명을 사용 (test001)
-        String username = "test001";
+        // 현재 로그인 된 사용자의 userId를 input함
+        String username = getCurrentUsername();
 
         // 1. userRepository에 pdf를 올린 유저의 정보가 업로드되어야 함.
         UserEntity userEntity = userRepository.findByName(username)
@@ -59,26 +62,36 @@ public class MainDao {
     }
 
     // pdf를 이미지로 변환하여 저장하는 메소드
-    private void convertPdfToImagesAndSave(MultipartFile pdfFile, Long resumeId) throws IOException {
+    private void convertPdfToImagesAndSave(MultipartFile pdfFile, Long resumeId) {
         try (PDDocument document = PDDocument.load(pdfFile.getInputStream())) {
             PDFRenderer pdfRenderer = new PDFRenderer(document);
-
+    
             // 이미지를 저장할 폴더 생성
             Path resumeFolder = Paths.get(uploadDirectory, String.valueOf(resumeId));
             resumeFolder.toFile().mkdirs();
-
+    
             for (int pageIndex = 0; pageIndex < document.getNumberOfPages(); pageIndex++) {
                 BufferedImage image = pdfRenderer.renderImageWithDPI(pageIndex, 300, ImageType.RGB);
-
+    
                 // 이미지를 저장
                 File outputImageFile = new File(resumeFolder.toFile(), resumeId + "_" + pageIndex + ".png");
                 ImageIO.write(image, "png", outputImageFile);
-                
             }
+        } catch (IOException e) {
+            e.printStackTrace(); // 예외 메시지를 콘솔에 출력 
+
         }
     }
 
-    
+    private String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            return ((UserDetails) authentication.getPrincipal()).getUsername();
+        }
+
+        return "test001"; 
+    }
 
     
 
--- heart/src/main/resources/static/js/viewer.js
+++ heart/src/main/resources/static/js/viewer.js
@@ -113,12 +113,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Reset 버튼 클릭 이벤트 핸들러
+    // Reset 버튼 클릭 이벤트 핸들러
     document.getElementById('resetButton').addEventListener('click', function() {
         // 모든 하트 스티커 제거
         var heartStickers = document.querySelectorAll('.draggable-sticker');
         heartStickers.forEach(function(heartSticker) {
             heartSticker.parentNode.removeChild(heartSticker);
         });
+
+        // 배열 초기화
+        heartLocations = [];
+
+        // 배열 정보 확인
+        console.log("하트 배열 초기화 완료", heartLocations);
     });
 
     // Save 버튼 클릭 이벤트 핸들러
